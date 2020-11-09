2a6829ddd5b44b41966ba19c5376490ab2b691d6
steliannechita <steliannechita2013@gmail.com>
2020-11-09 11:37:09 +0000

    change responsiveness of games images

--- src/styles/mainwrap/games.js
+++ src/styles/mainwrap/games.js
@@ -4,13 +4,11 @@ export const GamesWrapper = styled.div`
   width: 100%;
   display: flex;
   flex-wrap: wrap;
-  max-height: 372px;
   overflow-y: overlay;
   justify-content: space-between;
 
   img {
-    max-width: 43vmin;
-    height: auto;
+    max-width: 42vw;
     margin-bottom: 14px;
     &:hover {
       box-sizing: border-box;
@@ -18,16 +16,6 @@ export const GamesWrapper = styled.div`
       border-radius: 3px;
       cursor: pointer;
     }
-
-    /* @media only screen and (min-width: 280px) {
-      max-width: 135px;
-    }
-    @media only screen and (min-width: 361px) {
-      max-width: 160px;
-    }
-    @media only screen and (min-width: 576px) {
-      max-width: 175px;
-    } */
     @media screen and (min-width: 740px) {
       max-width: 190px;
     }
